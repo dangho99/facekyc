32e120846cd59c72636f977788e4de140e248ef0
hoangph3 <phamhoanghxh1@gmail.com>
2022-09-23 00:43:28 +0700

    test

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from keeper.environments import SystemEnv
 
 if __name__ == '__main__':
     # run api
-    run(api_host=SystemEnv.host, api_port=9980, debug=True)
+    run(api_host=SystemEnv.host, api_port=8999, debug=True)
