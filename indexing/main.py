981df26a2f113e4ad2308144994a06dbf54a37e8
hoangph3 <phamhoanghxh1@gmail.com>
2023-04-16 18:16:08 +0700

    update debug mode

--- indexing/main.py
+++ indexing/main.py
@@ -4,8 +4,12 @@ import os
 
 API_HOST = os.getenv("API_HOST", "127.0.0.1")
 API_PORT = int(os.getenv("API_PORT", "8999"))
+DEBUG = os.getenv("DEBUG", "")
     
 
 if __name__ == '__main__':
+    debug = False
+    if debug == "1":
+        debug = True
     # run api
-    run(api_host='0.0.0.0', api_port=API_PORT, debug=True)
+    run(api_host='0.0.0.0', api_port=API_PORT, debug=debug)
